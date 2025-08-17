c90bd92b4466fcaa1f8f45650754ac5ad9222a0a
benjabrezzo <benjabrezzo@gmail.com>
2025-08-17 19:23:57 -0300

    Se empezo con los ejercicios de algebra de bloques. Unidad 2.

---
+++ U2_E2.m
@@ -0,0 +1,28 @@
+# Unidad 2; Ejercicio 2
+pkg load control
+close all; clear all; clc;
+% Sistema 1
+s = tf('s');
+Kp = 31;
+G1 = (53*(s + 1))/((s+10)*(s+100));
+G2 = 1/s;
+Kd = 16;
+
+% Sin las re-alimentaciones
+FT0 = Kp*G1*G2;
+step(FT0);
+
+% Re-alimentación de G1 y Kd
+RealimG1 = feedback(G1,Kd);
+RealimG1 = feedback(G1,Kd,-1); # CONSULTA: Signo en feedback, re-alimentación + o -
+FT1 = Kp*RealimG1*G2;
+step(FT1);
+
+% Re-alimentación de G2
+RealimG2 = feedback(RealimG1,1);
+RealimG2 = feedback(RealimG1,1,-1); # CONSULTA: Signo en feedback, re-alimentación + o -
+step(RealimG2);
+
+
+
+disp('Terminado')
